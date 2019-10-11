89c566f76526c30a239cdd6980714829b93e767b
Ian Sweet <iannsweet@gmail.com>
2019-10-11 12:55:36 -0400

    relational spec done, need to prove it agrees with transl

--- cfrontend/ChkCgenspec.v
+++ cfrontend/ChkCgenspec.v
@@ -14,7 +14,7 @@
 
 Require Import Coqlib Maps Errors Integers Floats.
 Require Import AST Linking Memory.
-Require Import Ctypes Cop ChkCsyntax Clight ChkCgen.
+Require Import Ctypes Cop ChkCsyntax Csyntax ChkCgen.
 
 Section SPEC.
 
@@ -32,7 +32,7 @@ Local Open Scope gensym_monad_scope.
   any Clight expression [a] that evaluates to [v] in any environment
   matching the given temporary environment [le].
 *)
-
+(*
 Definition final (dst: destination) (a: expr) : list statement :=
   match dst with
   | For_val => nil
@@ -401,74 +401,143 @@ Inductive tr_if: Csyntax.expr -> statement -> statement -> statement -> Prop :=
   | tr_if_intro: forall r s1 s2 sl a tmps,
       (forall ge e le m, tr_top ge e le m For_val r sl a tmps) ->
       tr_if r s1 s2 (makeseq (sl ++ makeif a s1 s2 :: nil)).
+ *)
+Inductive tr_expr: ChkCsyntax.expr -> expr -> Prop :=
+| tr_val: forall v ty,
+    tr_expr (ChkCsyntax.Eval v ty) (Eval v ty)
+| tr_var: forall x ty,
+    tr_expr (ChkCsyntax.Evar x ty) (Evar x ty)
+| tr_field: forall l tl f ty,
+    tr_expr l tl ->
+    tr_expr (ChkCsyntax.Efield l f ty) (Efield tl f ty)
+| tr_valof: forall l tl ty,
+    tr_expr l tl ->
+    tr_expr (ChkCsyntax.Evalof l ty) (Evalof tl ty)
+| tr_deref: forall r tr ty,
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Ederef r ty) (Ederef tr ty)
+| tr_addrof: forall l tl ty,
+    tr_expr l tl ->
+    tr_expr (ChkCsyntax.Eaddrof l ty) (Eaddrof tl ty)
+| tr_unop: forall op r tr ty,
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Eunop op r ty) (Eunop op tr ty)
+| tr_binop: forall op r1 tr1 r2 tr2 ty,
+    tr_expr r1 tr1 ->
+    tr_expr r2 tr2 ->
+    tr_expr (ChkCsyntax.Ebinop op r1 r2 ty) (Ebinop op tr1 tr2 ty)
+| tr_cast: forall r tr ty,
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Ecast r ty) (Ecast tr ty)
+| tr_seqand: forall r1 tr1 r2 tr2 ty,
+    tr_expr r1 tr1 ->
+    tr_expr r2 tr2 ->
+    tr_expr (ChkCsyntax.Eseqand r1 r2 ty) (Eseqand tr1 tr2 ty)
+| tr_seqor: forall r1 tr1 r2 tr2 ty,
+    tr_expr r1 tr1 ->
+    tr_expr r2 tr2 ->
+    tr_expr (ChkCsyntax.Eseqor r1 r2 ty) (Eseqor tr1 tr2 ty)
+| tr_cond: forall r1 tr1 r2 tr2 r3 tr3 ty,
+    tr_expr r1 tr1 ->
+    tr_expr r2 tr2 ->
+    tr_expr r3 tr3 ->
+    tr_expr (ChkCsyntax.Econdition r1 r2 r3 ty) (Econdition tr1 tr2 tr3 ty)
+| tr_sizeof: forall ty' ty,
+    tr_expr (ChkCsyntax.Esizeof ty' ty) (Esizeof ty' ty)
+| tr_alignof: forall ty' ty,
+    tr_expr (ChkCsyntax.Ealignof ty' ty) (Ealignof ty' ty)
+| tr_assign: forall l tl r tr ty,
+    tr_expr l tl ->
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Eassign l r ty) (Eassign tl tr ty)
+| tr_assignop: forall op l tl r tr tyres ty,
+    tr_expr l tl ->
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Eassignop op l r tyres ty) (Eassignop op tl tr tyres ty)
+| tr_postincr: forall id l tl ty,
+    tr_expr l tl ->
+    tr_expr (ChkCsyntax.Epostincr id l ty) (Epostincr id tl ty)
+| tr_comma: forall r1 tr1 r2 tr2 ty,
+    tr_expr r1 tr1 ->
+    tr_expr r2 tr2 ->
+    tr_expr (ChkCsyntax.Ecomma r1 r2 ty) (Ecomma tr1 tr2 ty)
+| tr_call: forall r1 tr1 rargs trargs ty,
+    tr_expr r1 tr1 ->
+    tr_exprlist rargs trargs ->
+    tr_expr (ChkCsyntax.Ecall r1 rargs ty) (Ecall tr1 trargs ty)
+| tr_builtin: forall ef tyargs rargs trargs ty,
+    tr_exprlist rargs trargs ->
+    tr_expr (ChkCsyntax.Ebuiltin ef tyargs rargs ty) (Ebuiltin ef tyargs trargs ty)
+| tr_loc: forall b ofs ty,
+    tr_expr (ChkCsyntax.Eloc b ofs ty) (Eloc b ofs ty)
+| tr_paren: forall r tr tycast ty,
+    tr_expr r tr ->
+    tr_expr (ChkCsyntax.Eparen r tycast ty) (Eparen tr tycast ty)
 
-Inductive tr_stmt: Csyntax.statement -> statement -> Prop :=
+with tr_exprlist: ChkCsyntax.exprlist -> exprlist -> Prop :=
+| tr_nil:
+    tr_exprlist ChkCsyntax.Enil Enil
+| tr_cons: forall r1 tr1 rl trl,
+    tr_expr r1 tr1 ->
+    tr_exprlist rl trl ->
+    tr_exprlist (ChkCsyntax.Econs r1 rl) (Econs tr1 trl).
+
+Inductive tr_stmt: ChkCsyntax.statement -> statement -> Prop :=
   | tr_skip:
-      tr_stmt Csyntax.Sskip Sskip
-  | tr_do: forall r s,
-      tr_expr_stmt r s ->
-      tr_stmt (Csyntax.Sdo r) s
+      tr_stmt ChkCsyntax.Sskip Sskip
+  | tr_do: forall e te,
+      tr_expr e te ->
+      tr_stmt (ChkCsyntax.Sdo e) (Sdo te)
   | tr_seq: forall s1 s2 ts1 ts2,
-      tr_stmt s1 ts1 -> tr_stmt s2 ts2 ->
-      tr_stmt (Csyntax.Ssequence s1 s2) (Ssequence ts1 ts2)
-  | tr_ifthenelse_empty: forall r s' a,
-      tr_expression r s' a ->
-      tr_stmt (Csyntax.Sifthenelse r Csyntax.Sskip Csyntax.Sskip) (Ssequence s' Sskip)
-  | tr_ifthenelse: forall r s1 s2 s' a ts1 ts2,
-      tr_expression r s' a ->
-      tr_stmt s1 ts1 -> tr_stmt s2 ts2 ->
-      tr_stmt (Csyntax.Sifthenelse r s1 s2) (Ssequence s' (Sifthenelse a ts1 ts2))
-  | tr_while: forall r s1 s' ts1,
-      tr_if r Sskip Sbreak s' ->
       tr_stmt s1 ts1 ->
-      tr_stmt (Csyntax.Swhile r s1)
-              (Sloop (Ssequence s' ts1) Sskip)
-  | tr_dowhile: forall r s1 s' ts1,
-      tr_if r Sskip Sbreak s' ->
+      tr_stmt s2 ts2 ->
+      tr_stmt (ChkCsyntax.Ssequence s1 s2) (Ssequence ts1 ts2)
+  | tr_ifthenelse: forall e te s1 ts1 s2 ts2,
+      tr_expr e te ->
       tr_stmt s1 ts1 ->
-      tr_stmt (Csyntax.Sdowhile r s1)
-              (Sloop ts1 s')
-  | tr_for_1: forall r s3 s4 s' ts3 ts4,
-      tr_if r Sskip Sbreak s' ->
+      tr_stmt s2 ts2 ->
+      tr_stmt (ChkCsyntax.Sifthenelse e s1 s2) (Sifthenelse te ts1 ts2)
+  | tr_while: forall e te s1 ts1,
+      tr_expr e te ->
+      tr_stmt s1 ts1 ->
+      tr_stmt (ChkCsyntax.Swhile e s1) (Swhile te ts1)
+  | tr_dowhile: forall e te s1 ts1,
+      tr_expr e te ->
+      tr_stmt s1 ts1 ->
+      tr_stmt (ChkCsyntax.Sdowhile e s1) (Sdowhile te ts1)
+  | tr_for: forall s1 ts1 e2 te2 s3 ts3 s4 ts4,
+      tr_stmt s1 ts1 ->
+      tr_expr e2 te2 ->
       tr_stmt s3 ts3 ->
       tr_stmt s4 ts4 ->
-      tr_stmt (Csyntax.Sfor Csyntax.Sskip r s3 s4)
-              (Sloop (Ssequence s' ts4) ts3)
-  | tr_for_2: forall s1 r s3 s4 s' ts1 ts3 ts4,
-      tr_if r Sskip Sbreak s' ->
-      s1 <> Csyntax.Sskip ->
-      tr_stmt s1 ts1 ->
-      tr_stmt s3 ts3 ->
-      tr_stmt s4 ts4 ->
-      tr_stmt (Csyntax.Sfor s1 r s3 s4)
-              (Ssequence ts1 (Sloop (Ssequence s' ts4) ts3))
+      tr_stmt (ChkCsyntax.Sfor s1 e2 s3 s4) (Sfor ts1 te2 ts3 ts4)
   | tr_break:
-      tr_stmt Csyntax.Sbreak Sbreak
+      tr_stmt ChkCsyntax.Sbreak Sbreak
   | tr_continue:
-      tr_stmt Csyntax.Scontinue Scontinue
+      tr_stmt ChkCsyntax.Scontinue Scontinue
   | tr_return_none:
-      tr_stmt (Csyntax.Sreturn None) (Sreturn None)
-  | tr_return_some: forall r s' a,
-      tr_expression r s' a ->
-      tr_stmt (Csyntax.Sreturn (Some r)) (Ssequence s' (Sreturn (Some a)))
-  | tr_switch: forall r ls s' a tls,
-      tr_expression r s' a ->
+      tr_stmt (ChkCsyntax.Sreturn None) (Sreturn None)
+  | tr_return_some: forall e te,
+      tr_expr e te ->
+      tr_stmt (ChkCsyntax.Sreturn (Some e)) (Sreturn (Some te))
+  | tr_switch: forall e te ls tls,
+      tr_expr e te ->
       tr_lblstmts ls tls ->
-      tr_stmt (Csyntax.Sswitch r ls) (Ssequence s' (Sswitch a tls))
+      tr_stmt (ChkCsyntax.Sswitch e ls) (Sswitch te tls)
   | tr_label: forall lbl s ts,
       tr_stmt s ts ->
-      tr_stmt (Csyntax.Slabel lbl s) (Slabel lbl ts)
+      tr_stmt (ChkCsyntax.Slabel lbl s) (Slabel lbl ts)
   | tr_goto: forall lbl,
-      tr_stmt (Csyntax.Sgoto lbl) (Sgoto lbl)
+      tr_stmt (ChkCsyntax.Sgoto lbl) (Sgoto lbl)
 
-with tr_lblstmts: Csyntax.labeled_statements -> labeled_statements -> Prop :=
+with tr_lblstmts: ChkCsyntax.labeled_statements -> labeled_statements -> Prop :=
   | tr_ls_nil:
-      tr_lblstmts Csyntax.LSnil LSnil
+      tr_lblstmts ChkCsyntax.LSnil LSnil
   | tr_ls_cons: forall c s ls ts tls,
       tr_stmt s ts ->
       tr_lblstmts ls tls ->
-      tr_lblstmts (Csyntax.LScons c s ls) (LScons c ts tls).
-
+      tr_lblstmts (ChkCsyntax.LScons c s ls) (LScons c ts tls).
+(*
 (** * Correctness proof with respect to the specification. *)
 
 (** ** Properties of the monad *)
@@ -1071,19 +1140,19 @@ Opaque transl_expression transl_expr_stmt.
   induction s; simpl; intros until I; intros TR;
   monadInv TR; constructor; eauto.
 Qed.
-
+*)
 (** Relational presentation for the transformation of functions, fundefs, and variables. *)
 
-Inductive tr_function: Csyntax.function -> Clight.function -> Prop :=
+Inductive tr_function: ChkCsyntax.function -> Csyntax.function -> Prop :=
   | tr_function_intro: forall f tf,
-      tr_stmt f.(Csyntax.fn_body) tf.(fn_body) ->
-      fn_return tf = Csyntax.fn_return f ->
-      fn_callconv tf = Csyntax.fn_callconv f ->
-      fn_params tf = Csyntax.fn_params f ->
-      fn_vars tf = Csyntax.fn_vars f ->
+      tr_stmt f.(ChkCsyntax.fn_body) tf.(fn_body) ->
+      fn_return tf = ChkCsyntax.fn_return f ->
+      fn_callconv tf = ChkCsyntax.fn_callconv f ->
+      fn_params tf = ChkCsyntax.fn_params f ->
+      fn_vars tf = ChkCsyntax.fn_vars f ->
       tr_function f tf.
 
-Inductive tr_fundef: Csyntax.fundef -> Clight.fundef -> Prop :=
+Inductive tr_fundef: ChkCsyntax.fundef -> Csyntax.fundef -> Prop :=
   | tr_internal: forall f tf,
       tr_function f tf ->
       tr_fundef (Internal f) (Internal tf)
@@ -1096,7 +1165,7 @@ Lemma transl_function_spec:
   tr_function f tf.
 Proof.
   unfold transl_function; intros.
-  destruct (transl_stmt (Csyntax.fn_body f) (initial_generator tt)) eqn:T; inv H.
+  destruct (transl_stmt (ChkCsyntax.fn_body f) (initial_generator tt)) eqn:T; inv H.
   constructor; auto. simpl. eapply transl_stmt_meets_spec; eauto.
 Qed.
 
