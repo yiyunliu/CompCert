af513c4a1762bcb79539a95ec4a774ea16ddfc8b
Ian Sweet <iannsweet@gmail.com>
2019-10-24 17:39:02 -0400

    progress on forward simulation

--- cfrontend/ChkCgenproof.v
+++ cfrontend/ChkCgenproof.v
@@ -105,19 +105,301 @@ Definition measure (st: ChkCsem.state) : nat :=
   | _ => 0%nat
   end.
 
+Inductive match_ctx : (ChkCsyntax.expr -> ChkCsyntax.expr) -> (expr -> expr) -> Prop :=
+| match_ctx_transl: forall e e' te te' C tC,
+    tr_expr e te ->
+    C e = e' ->
+    tC te = te' ->
+    tr_expr e' te' ->
+    match_ctx C tC.
+
+Hint Constructors match_ctx.
+
+Inductive match_cont : ChkCsem.cont -> cont -> Prop :=
+  | match_Kstop:
+      match_cont ChkCsem.Kstop Kstop
+  | match_Kdo: forall k tk,
+      match_cont k tk ->
+      match_cont (ChkCsem.Kdo k) (Kdo tk)
+  | match_Kseq: forall s k ts tk,
+      tr_stmt s ts ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kseq s k) (Kseq ts tk)
+  | match_Kifthenelse: forall s1 s2 k ts1 ts2 tk,
+      tr_stmt s1 ts1 ->
+      tr_stmt s2 ts2 ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kifthenelse s1 s2 k)
+                 (Kifthenelse ts1 ts2 tk)
+  | match_Kwhile1: forall e te s ts k tk,
+      tr_expr e te ->
+      tr_stmt s ts ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kwhile1 e s k)
+                 (Kwhile1 te ts tk)
+  | match_Kwhile2: forall e te s ts k tk,
+      tr_expr e te ->
+      tr_stmt s ts ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kwhile2 e s k)
+                 (Kwhile2 te ts tk)
+  | match_Kdowhile1: forall e te s ts k tk,
+      tr_expr e te ->
+      tr_stmt s ts ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kdowhile1 e s k)
+                 (Kdowhile1 te ts tk)
+  | match_Kdowhile2: forall e te s ts k tk,
+      tr_expr e te ->
+      tr_stmt s ts ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kdowhile2 e s k)
+                 (Kdowhile2 te ts tk)
+  | match_Kfor2: forall e te s1 ts1 s2 ts2 k tk,
+      tr_expr e te ->
+      tr_stmt s1 ts1 ->
+      tr_stmt s2 ts2 ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kfor2 e s1 s2 k)
+                 (Kfor2 te ts1 ts2 tk)
+  | match_Kfor3: forall e te s1 ts1 s2 ts2 k tk,
+      tr_expr e te ->
+      tr_stmt s1 ts1 ->
+      tr_stmt s2 ts2 ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kfor3 e s1 s2 k)
+                 (Kfor3 te ts1 ts2 tk)
+  | match_Kfor4: forall e te s1 ts1 s2 ts2 k tk,
+      tr_expr e te ->
+      tr_stmt s1 ts1 ->
+      tr_stmt s2 ts2 ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kfor4 e s1 s2 k)
+                 (Kfor4 te ts1 ts2 tk)
+  | match_Kswitch1: forall ss tss k tk,
+      tr_lblstmts ss tss ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kswitch1 ss k)
+                 (Kswitch1 tss tk)
+  | match_Kswitch2: forall k tk,
+      match_cont k tk ->
+      match_cont (ChkCsem.Kswitch2 k)
+                 (Kswitch2 tk)
+  | match_Kreturn: forall k tk,
+      match_cont k tk ->
+      match_cont (ChkCsem.Kreturn k)
+                 (Kreturn tk)
+  | match_Kcall: forall f tf e C tC t k tk,
+      tr_function f tf ->
+      match_ctx C tC ->
+      match_cont k tk ->
+      match_cont (ChkCsem.Kcall f e C t k)
+                 (Kcall tf e tC t tk).
+
+Hint Constructors match_cont.
+
+Inductive match_states: ChkCsem.state -> state -> Prop :=
+| match_regstates: forall f tf s ts k tk e m,
+    tr_function f tf ->
+    tr_stmt s ts ->
+    match_cont k tk ->
+      match_states (ChkCsem.State f s k e m)
+                   (State tf ts tk e m)
+| match_exprstates: forall f tf e te k tk env m,
+    tr_function f tf ->
+    tr_expr e te ->
+    match_cont k tk ->
+      match_states (ChkCsem.ExprState f e k env m)
+                   (ExprState tf te tk env m)
+| match_callstates: forall fd tfd args k tk m,
+    tr_fundef fd tfd ->
+    match_cont k tk ->
+    match_states (ChkCsem.Callstate fd args k m)
+                 (Csem.Callstate tfd args tk m)
+| match_returnstates: forall res k tk m,
+    match_cont k tk ->
+    match_states (ChkCsem.Returnstate res k m)
+                 (Csem.Returnstate res tk m)
+| match_stuckstates:
+    match_states ChkCsem.Stuckstate Csem.Stuckstate.
+
+Hint Constructors match_states.
+
+Hint Constructors sstep.
+Hint Constructors estep.
+Hint Constructors tr_stmt.
+Hint Constructors tr_expr.
+
+Lemma transl_not_skip:
+  forall a a',
+    a <> ChkCsyntax.Sskip ->
+    tr_stmt a a' ->
+    a' <> Csyntax.Sskip.
+Proof.
+  intros.
+  induction H0; try discriminate.
+  destruct H. reflexivity.
+Qed.
+
+Hint Resolve transl_not_skip.
+
+Lemma transl_mem:
+  forall m e m',
+    Mem.free_list m (ChkCsem.blocks_of_env ge e) = Some m' ->
+    Mem.free_list m (Csem.blocks_of_env tge e) = Some m'.
+Proof.
+Admitted.
+
+Hint Resolve transl_mem.
+
+Lemma transl_f_cast:
+  forall v1 ty f tf m v2,
+    tr_function f tf ->
+    sem_cast v1 ty (ChkCsyntax.fn_return f) m = Some v2 ->
+    sem_cast v1 ty (Csyntax.fn_return tf) m = Some v2.
+Proof.
+Admitted.
+
+Hint Resolve transl_f_cast.
+
+Lemma transl_call_cont:
+  forall k tk,
+    match_cont k tk ->
+    match_cont (ChkCsem.call_cont k) (Csem.call_cont tk).
+Proof.
+  induction 1; try simpl; eauto.
+Qed.
+
+Hint Resolve transl_call_cont.
+
+Lemma transl_is_call_cont:
+  forall k tk,
+    match_cont k tk ->
+    ChkCsem.is_call_cont k ->
+    is_call_cont tk.
+Proof.
+  induction 1; eauto.
+Qed.
+
+Hint Resolve transl_is_call_cont.
+
+Lemma transl_lblstmts:
+  forall n ss tss,
+    tr_lblstmts ss tss ->
+    tr_stmt (ChkCsem.seq_of_labeled_statement (ChkCsem.select_switch n ss))
+            (seq_of_labeled_statement (select_switch n tss)).
+Proof.
+Admitted.
+
+Hint Resolve transl_lblstmts.
+
+Lemma transl_find_label:
+  forall lbl f tf k tk s' k' ts' tk',
+    tr_function f tf ->
+    match_cont k tk ->
+    ChkCsem.find_label
+      lbl (ChkCsyntax.fn_body f) (ChkCsem.call_cont k) = Some (s', k') ->
+    find_label lbl (fn_body tf) (call_cont tk) = Some (ts', tk').
+Proof.
+Admitted.
+
+Hint Resolve transl_find_label.
+
+Lemma estep_simulation:
+  forall S1 t S2, ChkCstrategy.estep ge S1 t S2 ->
+  forall S1' (MS: match_states S1 S1'),
+  exists S2', Cstrategy.step tge S1' t S2' /\ match_states S2 S2'.
+Proof.
+Admitted.
+
+Lemma sstep_simulation:
+  forall S1 t S2, ChkCsem.sstep ge S1 t S2 ->
+  forall S1' (MS: match_states S1 S1'),
+  exists S2', Cstrategy.step tge S1' t S2' /\ match_states S2 S2'.
+Proof.
+  induction 1; intros; inv MS.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H7. inv H8. destruct b; econstructor; split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H; inv H7; inv H8; econstructor; split; try right; eauto.
+  - inv H6. inv H7. econstructor; split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H; inv H7; inv H8; econstructor; split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H7. econstructor. split; try right; eauto.
+  - inv H6. inv H3. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H; inv H7; inv H8; econstructor; split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H7. econstructor. split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H8. inv H9. econstructor. split; try right; eauto.
+  - inv H8. econstructor. split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H7. inv H8. econstructor. split; try right; eauto.
+  - inv H; inv H7; inv H8; econstructor; split; try right; eauto.
+  - inv H6. inv H7. econstructor. split; try right; eauto.
+  - inv H6. econstructor. split; try right; eauto.
+  - inv H7. econstructor. split; try right; eauto. admit.
+  - inv H7. econstructor. split; try right; eauto. econstructor.
+    admit.
+    admit.
+    admit.
+    constructor.
+    assumption.
+    inv H3.
+    assumption.
+    assumption.
+  - admit.
+  - inv H3. econstructor. split; try right; eauto. admit.
+Qed.
+
+Lemma simulation:
+  forall S1 t S2, ChkCstrategy.step ge S1 t S2 ->
+             forall S1' (MS: match_states S1 S1'),
+             exists S2', Cstrategy.step tge S1' t S2' /\ match_states S2 S2'.
+Proof.
+  intros S1 t S2 STEP.
+  destruct STEP.
+  apply estep_simulation; auto.
+  apply sstep_simulation; auto.
+Qed.
+
+Lemma transl_initial_states:
+  forall S,
+    ChkCsem.initial_state prog S ->
+    exists S', Csem.initial_state tprog S' /\ match_states S S'.
+Proof.
+Admitted.
+
+Lemma transl_final_states:
+  forall S S' r,
+    match_states S S' -> ChkCsem.final_state S r -> Csem.final_state S' r.
+Proof.
+Admitted.
+
+
 Theorem transl_program_correct:
   forward_simulation (ChkCstrategy.semantics prog) (Cstrategy.semantics tprog).
 Proof.
-Admitted.
-(*
-  eapply forward_simulation_star_wf with (order := ltof _ measure).
+  eapply forward_simulation_step.
   eapply senv_preserved.
   eexact transl_initial_states.
   eexact transl_final_states.
-  apply well_founded_ltof.
   exact simulation.
 Qed.
-*)
 
 End PRESERVATION.
 
