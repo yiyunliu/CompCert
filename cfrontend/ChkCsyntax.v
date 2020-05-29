6fd5ce6d529e3c87c292540e7703f7b3d470e458
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-29 11:18:33 -0400

    change from Some t to Tret t

--- cfrontend/ChkCsyntax.v
+++ cfrontend/ChkCsyntax.v
@@ -103,10 +103,9 @@ Definition Epreincr (id: incr_or_decr) (l: expr) (ty: type) :=
 (** Selection is a conditional expression that always evaluates both arms
   and can be implemented by "conditional move" instructions.
   It is expressed as an invocation of a builtin function. *)
-
 Definition Eselection (r1 r2 r3: expr) (ty: type) :=
   let t := typ_of_type ty in
-  let sg := mksignature (AST.Tint :: t :: t :: nil) (Some t) cc_default in
+  let sg := mksignature (AST.Tint :: t :: t :: nil) (Tret t) cc_default in
   Ebuiltin (EF_builtin "__builtin_sel"%string sg)
            (Tcons type_bool (Tcons ty (Tcons ty Tnil)))
            (Econs r1 (Econs r2 (Econs r3 Enil)))
